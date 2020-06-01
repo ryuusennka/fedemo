c4a806a80e0ece3d092f9d4ac2291b6f835ac334
Sennka Ryuu <sennkaryuu@Sennkas-MacBook-Pro.local>
2020-06-01 13:16:25 +0800

    update

---
+++ page/01/index.babel.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: ryuusennka
+ * @Date: 2020-06-01 11:10:22
+ * @LastEditors: ryuusennka
+ * @LastEditTime: 2020-06-01 13:11:14
+ * @FilePath: /fedemo/page/01/index.babel.js
+ * @Description:
+ */
+
+const a = () => 1010;
+var div = document.createElement('h2');
+div.innerHTML = 'hello world!';
+document.body.appendChild(div);
