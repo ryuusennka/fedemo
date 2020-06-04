d95b1bc4850341f237d742af06fd8ff6e90c9ae0
Sennka Ryuu <sennkaryuu@Sennkas-MacBook-Pro.local>
2020-06-04 15:38:08 +0800

    Updates

---
+++ loading-01-strip/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+ * @Author: ryuusennka
+ * @Date: 2020-06-01 11:10:22
+ * @LastEditors: ryuusennka
+ * @LastEditTime: 2020-06-01 13:11:14
+ * @FilePath: /fedemo/page/01/index.babel.js
+ * @Description:
+ */
+var a = function a() {
+  return 1010;
+};
+
+var div = document.createElement('h2');
+div.innerHTML = 'hello world!';
+document.body.appendChild(div);
